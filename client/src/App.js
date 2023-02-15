a75441dbb3f437f4932e44d0b3d9f1de276e3408
12shussein <drahimhussein@gmail.com>
2023-02-15 20:29:17 +0000

    completed register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import "./App.css";
+import Register from './Register';
+import Login from './Login';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>CYF Attendance register</h1>
-      </header>
-    </div>
+    <main className="App">
+      <Login />
+    </main>
   );
 }
 
