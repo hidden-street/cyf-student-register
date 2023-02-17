afac848c7f233d7749e12a3465ebcf5abfb6f1d7
Dewayne Sewell <dewaynesewell1@gmail.com>
2023-02-17 06:23:43 +0000

    crud endpoints

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-
+const axios = require('axios');
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require("path");
@@ -47,12 +47,114 @@ const apiLimiter = rateLimit({
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   })
 
+
+  // Define a new route that retrieves data from the MongoDB database and returns it as a response.
+app.get('/api/logs', async (req, res) => {
+  try {
+    const Log = mongoose.model('Log', {
+      ID: String,
+      loginTime: Date,
+      logoutTime: Date,
+    });
+    const logs = await Log.find();
+    res.json(logs);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 // put routes here for now - I have already created user sign up and login look in controllers/ routes and middleware
 // get users
+const Student = mongoose.model('Student', {
+  username: String,
+  _id: mongoose.Schema.Types.ObjectId,
+  email: String,
+  cohort: String
+});
+
+// Define a new route that creates a new student document.
+app.post('/api/students', async (req, res) => {
+  try {
+    const student = new Student(req.body);
+    await student.save();
+    res.status(201).json({ message: 'Student created', student });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Define a new route that retrieves all student documents and returns them as a response.
+app.get('/api/students', async (req, res) => {
+  try {
+    const students = await Student.find();
+    res.json(students);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 // get user by id
-// get user by type
-// update user
-// delete user
+// Define a new route that retrieves a student document by ID and returns it as a response.
+app.get('/api/students/:id', async (req, res) => {
+  try {
+    const student = await Student.findById(req.params.id);
+    if (!student) {
+      return res.status(404).json({ error: 'Student not found' });
+    }
+    res.json(student);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// get user by username
+app.get('/api/students/username/:username', async (req, res) => {
+  try {
+    const student = await Student.findOne({ username: req.params.username });
+    if (!student) {
+      return res.status(404).json({ error: 'Student not found' });
+    }
+    res.json(student);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+// Define a new route that updates a student document by ID and returns it as a response.
+app.put('/api/students/:id', async (req, res) => {
+  try {
+    const student = await Student.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!student) {
+      return res.status(404).json({ error: 'Student not found' });
+    }
+    res.json(student);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+// Define a new route that deletes a student document by ID.
+app.delete('/api/students/:id', async (req, res) => {
+  try {
+    const student = await Student.findByIdAndDelete(req.params.id);
+    if (!student) {
+      return res.status(404).json({ error: 'Student not found' });
+    }
+    res.json({ message: 'Student deleted' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 // create user
 
 // create class
